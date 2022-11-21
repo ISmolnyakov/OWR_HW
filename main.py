71e33618e8e0e1cadbb4b23616247ae3224a3c0d
ISmolnyakov <smolnyakov.igor@gmail.com>
2022-11-21 22:41:28 +0700

    code fix
    extra lines deleted
    4.txt added for extra tests

--- main.py
+++ main.py
@@ -9,9 +9,6 @@ class Files:
     def __gt__(self, other):
         return self.count_lines() > other.count_lines()
 
-    def __lt__(self, other):
-        return self.count_lines() < other.count_lines()
-
     def write_file(self, other):
         with open('new_result.txt', 'w', encoding='utf-8') as result, open(self.item, encoding='utf-8') as first_file, \
                 open(other.item, encoding='utf-8') as second_file:
@@ -27,34 +24,13 @@ class Files:
             second_file.close()
             result.close()
 
-    # if file_one > file_two:
-    #     with open('new_result.txt', 'a', encoding='utf-8') as result:
-    #         result.write(f'{file_one.file}\n{file_one.count_lines()}\n{file_one.read()}')
-    #         result.write(f'{file_two.file}\n{file_two.count_lines()}\n{file_two.read()}')
-    # else:
-    #     with open('new_result.txt', 'a', encoding='utf-8') as result:
-    #         result.write(f'{file_two.file}\n{file_two.count_lines()}\n{file_two.read()}')
-    #         result.write(f'{file_one.file}\n{file_one.count_lines()}\n{file_one.read()}')
-    # return
-
 
 first = Files('1.txt')
 second = Files('2.txt')
 third = Files('3.txt')
 forth = Files('4.txt')
 # first.write_file(second)
-third.write_file(first)
+# third.write_file(first)
+forth.write_file(second)
 log = open('new_result.txt', encoding='utf-8')
 print(log.read())
-
-# print(f"Number of lines in {first.file} {'>' if first.count_lines() > second.count_lines() else '<'} "
-#       f"than in {second.file} and equal to { {first.count_lines()} if first.count_lines() > second.count_lines() else second.count_lines() } lines")
-
-
-
-
-
-
-
-
-
