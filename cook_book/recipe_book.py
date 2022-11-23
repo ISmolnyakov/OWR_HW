25623683ff02e9e17bd09ae3066745307222c6e4
ISmolnyakov <smolnyakov.igor@gmail.com>
2022-11-23 15:46:12 +0700

    Dir fix
    directories added

---
+++ cook_book/recipe_book.py
@@ -0,0 +1,47 @@
+from pprint import pprint
+
+
+class Cookbook:
+    def __init__(self, document):
+        self.document = document
+        self.cook_book = {}
+
+    def make_cook_book(self):
+        with open(self.document, encoding='utf-8') as file:
+            for lines in file:
+                item = lines.strip()
+                amount = int(file.readline())
+                food_list = []
+                for i in range(amount):
+                    ingr = file.readline().split(' | ')
+                    ingredients = {'ingredient_name': ingr[0], 'quantity': ingr[1], 'measure': ingr[2].strip()}
+                    food_list.append(ingredients)
+                self.cook_book[item] = food_list
+                file.readline()
+        book = open('recipe_dict.txt', 'w', encoding='utf-8')
+        book.write(f'{self.cook_book}')
+        book.close()
+        return self.cook_book
+
+
+def get_shop_list_by_dishes(dishes: list, res, person_count=2):
+    cook_book = res.make_cook_book()
+    shop_list = {}
+    for dish_name in dishes:
+        for ingredient in cook_book.get(dish_name, []):
+            if ingredient['ingredient_name'] in cook_book:
+                shop_list[ingredient['ingredient_name']]['quantity'] += int(ingredient['quantity']) * person_count
+            else:
+                shop_list[ingredient['ingredient_name']] = {'quantity': int(ingredient['quantity']) * person_count, 'measure': ingredient['measure']}
+    shop_list_file = open('shopping_list.txt', 'w', encoding='utf-8')
+    shop_list_file.write(f'{shop_list}')
+    shop_list_file.close()
+    return shop_list
+
+
+recipe = Cookbook('recipe.txt')
+pprint(recipe.make_cook_book(), width=70, sort_dicts=False)
+print()
+dish_list = ['Омлет', 'Фахитос']
+recipe_dict = open('recipe_dict.txt', 'r', encoding='utf-8')
+pprint(get_shop_list_by_dishes(dish_list, recipe), width=70)
---
+++ file_rewrite/File_rewrite.py
@@ -0,0 +1,36 @@
+class Files:
+    def __init__(self, item):
+        self.item = item
+
+    def count_lines(self):
+        lines_count = sum(1 for line in open(self.item))
+        return lines_count
+
+    def __gt__(self, other):
+        return self.count_lines() > other.count_lines()
+
+    def write_file(self, other):
+        with open('new_result.txt', 'w', encoding='utf-8') as result, open(self.item, encoding='utf-8') as first_file, \
+                open(other.item, encoding='utf-8') as second_file:
+            if Files.__gt__(self, other):
+                result.write(f'{second_file.name}\n{other.count_lines()}\n{second_file.read()}\n')
+                result.write(f'---------\n')
+                result.write(f'{first_file.name}\n{self.count_lines()}\n{first_file.read()}')
+            else:
+                result.write(f'{first_file.name}\n{self.count_lines()}\n{first_file.read()}\n')
+                result.write(f'---------\n')
+                result.write(f'{second_file.name}\n{other.count_lines()}\n{second_file.read()}')
+            first_file.close()
+            second_file.close()
+            result.close()
+
+
+first = Files('1.txt')
+second = Files('2.txt')
+third = Files('3.txt')
+forth = Files('4.txt')
+first.write_file(forth)
+# third.write_file(first)
+# forth.write_file(second)
+log = open('new_result.txt', encoding='utf-8')
+print(log.read())
