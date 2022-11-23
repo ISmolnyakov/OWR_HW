90de2fe1721533a909cebde4785a5aa086cb3b88
ISmolnyakov <smolnyakov.igor@gmail.com>
2022-11-23 15:41:58 +0700

    Cook Book HW done
    done

--- recipe_book.py
+++ recipe_book.py
@@ -1,36 +1,47 @@
 from pprint import pprint
 
-class Cook_book:
+
+class Cookbook:
     def __init__(self, document):
         self.document = document
         self.cook_book = {}
 
-    def open_file(self):
-        data = open(self.document, 'r', encoding='utf-8')
-        return data.readlines()
-
-    def make_cook_book_keys(self):
-        for info in self.open_file():
+    def make_cook_book(self):
+        with open(self.document, encoding='utf-8') as file:
+            for lines in file:
+                item = lines.strip()
+                amount = int(file.readline())
+                food_list = []
+                for i in range(amount):
+                    ingr = file.readline().split(' | ')
+                    ingredients = {'ingredient_name': ingr[0], 'quantity': ingr[1], 'measure': ingr[2].strip()}
+                    food_list.append(ingredients)
+                self.cook_book[item] = food_list
+                file.readline()
+        book = open('recipe_dict.txt', 'w', encoding='utf-8')
+        book.write(f'{self.cook_book}')
+        book.close()
         return self.cook_book
 
 
+def get_shop_list_by_dishes(dishes: list, res, person_count=2):
+    cook_book = res.make_cook_book()
+    shop_list = {}
+    for dish_name in dishes:
+        for ingredient in cook_book.get(dish_name, []):
+            if ingredient['ingredient_name'] in cook_book:
+                shop_list[ingredient['ingredient_name']]['quantity'] += int(ingredient['quantity']) * person_count
+            else:
+                shop_list[ingredient['ingredient_name']] = {'quantity': int(ingredient['quantity']) * person_count, 'measure': ingredient['measure']}
+    shop_list_file = open('shopping_list.txt', 'w', encoding='utf-8')
+    shop_list_file.write(f'{shop_list}')
+    shop_list_file.close()
+    return shop_list
 
-def get_shop_list_by_dishes(name, count):
-    pass
 
-
-recipe = Cook_book('recipe.txt')
-# recipe.make_cook_book()
-recipe.open_file()
-print(recipe.open_file())
-recipe.make_cook_book_keys()
-print(recipe.make_cook_book_keys())
-# get_shop_list_by_dishes(dishes, person_count)
-
-cook_book = {
-  'Омлет': [
-    {'ingredient_name': 'Яйцо', 'quantity': 2, 'measure': 'шт.'},
-    {'ingredient_name': 'Молоко', 'quantity': 100, 'measure': 'мл'},
-    {'ingredient_name': 'Помидор', 'quantity': 2, 'measure': 'шт'}
-    ]
-}
+recipe = Cookbook('recipe.txt')
+pprint(recipe.make_cook_book(), width=70, sort_dicts=False)
+print()
+dish_list = ['Омлет', 'Фахитос']
+recipe_dict = open('recipe_dict.txt', 'r', encoding='utf-8')
+pprint(get_shop_list_by_dishes(dish_list, recipe), width=70)
