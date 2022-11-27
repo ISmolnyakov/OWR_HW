e9279092ab34475fa2b680933b66ae6b71e235d9
ISmolnyakov <smolnyakov.igor@gmail.com>
2022-11-27 20:12:51 +0700

    tutor solution added

---
+++ file_rewrite/re_write(tutor_solution).py
@@ -0,0 +1,25 @@
+import os
+
+path = '../file_rewrite'
+
+
+def rewrite_file(path):
+    file_list = os.listdir(path)
+    info = {}
+    for content in file_list:
+        if content.rfind('.txt', -4) >= 0:
+            with open(os.path.join(path, content), 'r', encoding='utf-8') as file:
+                info[content] = file.readlines()
+
+    with open('../file_rewrite/resulting_file.txt', 'w', encoding='utf-8') as file:
+        for file_name, rows in sorted( info.items(), key= lambda x: len(x[1])):
+            file.write( file_name + '\n')
+            file.write(str(len(rows)) + '\n')
+            if '\n' not in rows[-1]:
+                rows[-1] += '\n'
+                file.write(''.join(rows))
+
+
+rewrite_file(path)
+log = open('../file_rewrite/resulting_file.txt', encoding='utf-8')
+print(log.read())
