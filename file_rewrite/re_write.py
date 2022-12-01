3561b07da4095fec98e3170ba1cbe64aa880d2ff
ISmolnyakov <smolnyakov.igor@gmail.com>
2022-12-01 16:29:17 +0700

    solution added

--- file_rewrite/re_write.py
+++ file_rewrite/re_write.py
@@ -13,7 +13,7 @@ def rewrite_file(path):
 
     with open('../file_rewrite/resulting_file.txt', 'w', encoding='utf-8') as file:
         for file_name, rows in sorted(info.items(), key=lambda x: len(x[1])):
-            file.write( file_name + '\n')
+            file.write(file_name + '\n')
             file.write(str(len(rows)) + '\n')
             if '\n' not in rows[-1]:
                 rows[-1] += '\n'
